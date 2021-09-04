64d5584f06e1dd05fa9be766b4e05e9ccdc4587f
PauloRutkoski <paulo.rtkoski@gmail.com>
2021-09-04 13:50:52 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/rutkoski/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/rutkoski/hrpayroll/service/PaymentService.java
@@ -1,12 +1,31 @@
 package com.rutkoski.hrpayroll.service;
 
 import com.rutkoski.hrpayroll.model.Payment;
+import com.rutkoski.hrpayroll.model.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
-    public Payment getPayment(long workerId, int days){
-        Payment payment = new Payment("Bob", 200.0, days);
-        return payment;
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(Long workerId, int days){
+        Map<String, String> paremeters = new HashMap<>();
+        paremeters.put("id", workerId.toString());
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, paremeters);
+        if(worker == null){
+            return null;
+        }
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
