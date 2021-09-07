fab3921e9c1febe98d6e681a703677857dd8b2fa
PauloRutkoski <paulo.rtkoski@gmail.com>
2021-09-07 11:02:56 -0300

    Config server

--- hr-worker/src/main/java/com/rutkoski/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/rutkoski/hrworker/controller/WorkerController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +22,20 @@ import com.rutkoski.hrworker.repositories.WorkerRepository;
 public class WorkerController {
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 	
     @Autowired
     private WorkerRepository repository;
+    
+    @GetMapping(value="/configs")
+    public ResponseEntity<Void> getConfigs(){
+       logger.info("CONFIG = " + testConfig);
+       return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
